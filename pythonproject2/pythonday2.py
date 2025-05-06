8572d9fc3b6f87f54f0ff7d0cf615e5e842229f5
Venkat-Sai-Neeldha <144706211+Venkat-Sai-Neeldha@users.noreply.github.com>
2025-05-06 12:37:00 +0530

    changes paython day 2

--- pythonproject2/pythonday2.py
+++ pythonproject2/pythonday2.py
@@ -4,24 +4,26 @@ print(str1[::-1])
 
 #Check if a string is a palindrome
 str2="madam"
-str3=str2[::-1]
-if str3==str2:
+if str2==str2[::-1]:
     print("palindrome")
 else:
     print("not palindrome")
 
 #Count vowels and consonants in a string
 
-vowels=['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
-vowelcount=0
-consonantcount=0
-str4="python"
+vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
+vowelcount = 0
+consonantcount = 0
+str4 = "python"
+
 for i in str4:
-    if i in vowels:
-        vowelcount+=1
-    else:
-        consonantcount+=1
-print(f"number of vowels: {vowelcount} and number of consonants: {consonantcount}")
+    if i.isalpha():
+        if i in vowels:
+            vowelcount += 1
+        else:
+            consonantcount += 1
+
+print(f"Number of vowels: {vowelcount} and number of consonants: {consonantcount}")
 
 
 #Find the frequency of each character
@@ -46,11 +48,8 @@ print(str5)
 
 #Extract digits from a string
 
-str6="abc123def"
-str7=""
-for char in str6:
-    if char.isnumeric():
-        str7=str7+char
+str6 = "abc123def"
+str7 = "".join([char for char in str6 if char.isnumeric()])
 print(str7)
 
 #Check if a string contains only alphabets
